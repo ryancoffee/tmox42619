5b3a8f8efdcc691efb89089027124bc32404bcd9
Ryan Coffee <coffeer76@gmail.com>
2021-07-03 14:55:17 -0700

    incremental, hit addressing is wrong

--- src/make2Dhist.py
+++ src/make2Dhist.py
@@ -4,10 +4,11 @@ import h5py
 import numpy as np
 import sys
 from scipy.interpolate import interp1d 
+import re
 
 def fill2dhist(csum):
     x = csum
-    y = np.arange(x.shape[1],dtype=np.uint8) # inflating by facotr of 4
+    y = np.arange(x.shape[1],dtype=np.uint8) 
     h = np.zeros(x.shape,dtype=np.int8)
     for i in range(x.shape[0]):
         cs = {}
@@ -28,20 +29,47 @@ def main():
         return
     fnames = list(sys.argv[1:])
     data = {}
+    fullname = sys.argv[1]
+    datadir = './'
+    filefront = 'hits.h5'
 
     for fname in fnames:
         #with h5py.File('./data_fs/h5files/%s'%(fname),'r') as f:
-        with h5py.File('%s'%(fname),'r') as f:
+        m = re.search('^(.+)/h5files/(.+).h5',fname)
+        if m:
+            fullname = m.group(0)
+            datadir = m.group(1)
+            filefront = m.group(2)
+            print('processing:\t%s'%fullname)
+        else:
+            return
+        with h5py.File('%s'%(fullname),'r') as f:
             print(list(f.keys()))
             for key in f.keys():
                 data[key] = np.squeeze(f[key][()])
         # here now the data is stored in system memory and we close the h5 file
+        vlsmean = np.mean(data['vls'],axis=0)
         vlscsum = np.cumsum(data['vls'],axis=1)
         h = fill2dhist(vlscsum)
+        tof = data['port_12_tofs']
+        tofaddresses = data['port_12_addresses']
+        tofnedges= data['port_12_nedges']
+        print(tof.shape)
+        print('\t'.join((tof[:20]-25000).astype(str)))
+        print('\t'.join(tofaddresses[:20].astype(str)))
+        print('\t'.join(tofnedges[:20].astype(str)))
 
-        np.savetxt('./data_fs/ascii/origvls.dat',data['vls'][:10,:].T,fmt='%i')
-        np.savetxt('./data_fs/ascii/tmpvls.dat',vlscsum[:10,:].T,fmt='%i')
-        np.savetxt('./data_fs/ascii/hist.dat',h.T,fmt='%i')
+        tofhist = np.zeros((2**16,vlsmean.shape[0]),dtype=np.int8)
+        print(tofhist.shape)
+        return
+
+        x = vlscsum
+        y = np.arange(x.shape[1],dtype=np.uint8) 
+
+
+        np.savetxt('%s/ascii/%s.origvls.dat'%(datadir,filefront),data['vls'][:10,:].T,fmt='%i')
+        np.savetxt('%s/ascii/%s.tmpvls.dat'%(datadir,filefront),vlscsum[:10,:].T,fmt='%i')
+        np.savetxt('%s/ascii/%s.hist.dat'%(datadir,filefront),h.T,fmt='%i')
     return
 
 
