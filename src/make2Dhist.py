fixing off-by-one addressing

src/make2Dhist.py
@@ -6,6 +6,21 @@ import sys
 from scipy.interpolate import interp1d 
 import re
 
+def samplePDF(csum,nsamples = 10):
+    b = []
+    x = csum
+    y = np.arange(x.shape[1],dtype=np.uint8) 
+    for i in range(x.shape[0]):
+        cs = {}
+        for j in range(x.shape[1]):
+            cs.update({x[i,j]:y[j]})
+        xx = [v for v in cs.keys()]
+        yy = [v for v in cs.values()]
+        f = interp1d(xx,yy)
+        xnew = np.random.random((nsamples,))*(xx[-2]-xx[1]) + xx[1]
+        b.append(list(f(xnew).astype(np.uint16)))
+    return b
+
 def fill2dhist(csum):
     x = csum
     y = np.arange(x.shape[1],dtype=np.uint8) 
@@ -18,7 +33,7 @@ def fill2dhist(csum):
         yy = [v for v in cs.values()]
         f = interp1d(xx,yy)
         xnew = np.random.random((100,))*(xx[-2]-xx[1]) + xx[1]
-        ynew = f(xnew).astype(np.uint8)
+        ynew = f(xnew).astype(np.uint16)
         for k in ynew:
             h[i,k] += 1
     return h
@@ -51,13 +66,20 @@ def main():
         vlsmean = np.mean(data['vls'],axis=0)
         vlscsum = np.cumsum(data['vls'],axis=1)
         h = fill2dhist(vlscsum)
+
+        spinds = samplePDF(vlscsum,10) # repeate this for every tof individually
         tof = data['port_14_tofs']
         tofaddresses = data['port_14_addresses']
         tofnedges= data['port_14_nedges']
         print(tof.shape)
-        print('\t'.join((tof[:20]-25000).astype(str)))
-        print('\t'.join(tofaddresses[:20].astype(str)))
-        print('\t'.join(tofnedges[:20].astype(str)))
+        print('tofs:\t\t'+'\t\t'.join((tof[:10]).astype(str)))
+        print('adds:\t\t'+'\t\t'.join(tofaddresses[:10].astype(str)))
+        print('edges:\t\t'+'\t\t'.join(tofnedges[:10].astype(str)))
+
+        for i in range(10):
+            if tofnedges[i] > 0:
+                toflist = tof[ int(tofaddresses[i]):int(tofaddresses[i]+tofnedges[i]) ].astype(np.uint32)
+                print(i,toflist)
 
         tofhist = np.zeros((2**16,vlsmean.shape[0]),dtype=np.int8)
         print(tofhist.shape)