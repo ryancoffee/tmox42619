27b7ee7c99e51061a614a3065815e1fb19e2397b
Ryan Coffee <coffeer76@gmail.com>
2022-01-26 09:22:19 -0800

    lassotest.old

--- sandbox/lassotest.py
+++ sandbox/lassotest.py
@@ -34,51 +34,52 @@ def idct2(x):
 def idct2_rnc(x):
   return spfft.idct(spfft.idct(x, axis=0),axis=1)
 
+def gen_dct_mat(veclen=128,bits=4):
+    return (dct(np.identity(veclen))*2**int(bits-1)+0.5).astype(int)
 
 def main():
     alphalist = [0.1, 0.05, 0.025, 0.01, 0.005, 0.0025, 0.001]
     nx=ny=16
     X = genImage(nc=4,nx=nx,ny=ny)
-    k = round(nx * ny * 0.5) # 50% sample
+    #W = genWave(nc=4,nx=nx)
+    k = round(nx * ny * 0.95) # 50% sample
     ri = np.random.choice(nx * ny, k, replace=False) # random sample of indices
     print(X)
     #b = X.T.flat[ri]
     #b = np.expand_dims(b, axis=1)
     #blist = [b for i in range(len(alphalist))]
-    blist = [np.expand_dims(X.T.flat[ri],axis=1) for i in range(len(alphalist))]
-    Alist = [ np.kron(
+    b = np.expand_dims(X.T.flat[ri],axis=1)
+    A = np.kron(
         spfft.idct(np.identity(nx), norm='ortho', axis=0),
         spfft.idct(np.identity(ny), norm='ortho', axis=0)
-        ) for i in range(len(alphalist))]
-    for i in range(len(alphalist)):
-        Alist[i] = Alist[i][ri,:] # same as phi times kron
+        )
+    Asparse = A[ri,:] # same as phi times kron
 
-    lassolist = []
-    lassolist += [Lasso(a) for a in alphalist]
-    _ = [lassolist[i].fit(Alist[i],blist[i]) for i in range(len(lassolist))]
+    lassolist = [Lasso(a) for a in alphalist]
+    lassolist = [lasso.fit(Asparse,b) for lasso in lassolist]
 
     Xatlist = [np.array(l.coef_).reshape(nx, ny).T for l in lassolist] # stack columns
     #Xat[:, ny//2:ny] = 0
     # Get the reconstructed image
-    print(np.max(Xatlist[0]))
     Xalist = [idct2_rnc(Xat) for Xat in Xatlist]
     fig = plt.figure()
-    imlist = []
-    Xatlist = []
-    Xalist = []
-    for i in range(len(Xalist)):
-        imlist += ['im'%i]
-        Xatlist += ['Xat%i'%(i)]
-        Xalist += ['Xa%i'%(i)]
-        axstr = '%i%i%i'%(len(Xalist),3,i+1)
-        imlist += fig.add_subplot(axstr)
-        axstr = '%i%i%i'%(len(Xalist),3,i+2)
-        Xatlist += fig.add_subplot(axstr)
-        axstr = '%i%i%i'%(len(Xalist),3,i+3)
-        Xalist += fig.add_subplot(axstr)
-        imlist[-1].imshow(X)
-        Xatlist[-1].imshow(Xatlist[i])
-        Xalist[-1].imshow(Xalist[i])
+    ax =[]
+    ax += [fig.add_subplot(331)]
+    ax += [fig.add_subplot(332)]
+    ax += [fig.add_subplot(333)]
+    ax += [fig.add_subplot(334)]
+    ax += [fig.add_subplot(335)]
+    ax += [fig.add_subplot(336)]
+    ax += [fig.add_subplot(337)]
+    ax += [fig.add_subplot(338)]
+    ax += [fig.add_subplot(339)]
+  
+    for i in range(3):#len(Xalist)):
+        print(i)
+        ax[i*3].imshow(X)
+        ax[i*3+1].imshow(Xatlist[i])
+        ax[i*3+2].imshow(Xalist[i])
+    fig.show()
     return
 
 if __name__ == '__main__':
