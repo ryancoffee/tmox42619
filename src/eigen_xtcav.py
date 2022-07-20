cb403fe7695112effb03d1dd1c690468484ce075
Ryan Coffee <coffee@slac.stanford.edu>
2022-07-20 10:19:19 -0700

    moving to multiprocessing

--- src/eigen_xtcav.py
+++ src/eigen_xtcav.py
@@ -5,6 +5,7 @@ import sys
 import re
 import h5py
 from scipy.fftpack import dct
+import multiprocessing as mp
 
 '''
 data = np.array([images[i,:,:].flatten() for i in range(images.shape[0])])
@@ -16,23 +17,40 @@ vals,vecs = np.linalg.eig(COVMAT) # this peice takes a long time.
 Then remove mean, then remove a handfull of np.dot(eigvecs,data) and plot the >0 pixels
 '''
 
+def processBlock(params):
+    with h5py.File('%s/%s.h5'%(params['inpath'],params['infile']),'r') as inh5:
+        (nims,xdim,ydim) = inh5['xtcav']['images'][()].shape 
+        nims //= params['nblocks'] 
+        print('newshape = (%i,%i,%i)'%(nims,xdim,ydim))
+        datablock = np.column_stack([inh5['xtcav']['images'][()][params['tid']+params['nblocks']*j,:,:].flatten() for j in range(nims) ] )
+        print('datablock %i shape\t%s'%(params['tid'],datablock.shape))
+    outfile = '%s.eigenxtcav.tid%i.h5'%(params['infile'],params['tid'])
+    params['outfile'] = outfile
+    covMat = np.cov(datablock)
+    print('covMat.shape',covMat.shape)
+    eigMat,eigVals = np.linalg.eig(covMat)
+    print(eigMat.shape)
+    return params
+
 def main():
     if len(sys.argv) < 3:
         print('Syntax: eigen_xtcav.py <h5 full path and name> <outputpath>')
         return
     fullinfile = sys.argv[1]
-    outpath = sys.argv[2]
     m = re.search('^(.+)/(.+)\.h5$',fullinfile)
     if not m:
         print(fullinfile)
         return
-    print('%s\t%s'%(m.group(1),m.group(2)))
-    inpath = m.group(1)
-    infile = m.group(2)
-    with h5py.File('%s/%s'%(inpath,infile)) as inh5:
-        datablock = []
-        for i in range(10):
-            datablock += [inh5['xtcav']['images'][()][i::10,:,:] ]
+    paramslist = [{} for i in range(10)]
+    for i,p in enumerate(paramslist):
+        p['inpath'] = str(m.group(1))
+        p['infile'] = str(m.group(2))
+        p['outpath'] = str(sys.argv[2])
+        p['nblocks'] = np.uint8(100)
+        p['tid'] = np.uint8(i)
+
+    pool = mp.Pool(len(paramslist))
+    returnlist = pool.map(processBlock,paramslist)
     return
 
 if __name__ == '__main__':
