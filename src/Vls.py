cdf6e926f24edabdec3ee7a815513fc7eac16706
Ryan Coffee <coffee@slac.stanford.edu>
2022-07-23 21:30:12 -0700

    VlsType using typing

--- src/Vls.py
+++ src/Vls.py
@@ -1,8 +1,13 @@
 import numpy as np
+import typing
+
+IntArray = list[int]
+
+VlsType = typing.NewType('VlsType',type(Vls))
 
 
 class Vls:
-    def __init__(self):
+    def __init__(self: VlsType) -> None:
         self.v = []
         self.vsize = int(0)
         self.vc = []
@@ -10,7 +15,7 @@ class Vls:
         self.initState = True
         return
 
-    def process(self, vlswv):
+    def process(self: VlsType, vlswv: IntArray) -> VlsType:
         #print("processing vls",vlswv.shape[0])
         num = np.sum(np.array([i*vlswv[i] for i in range(len(vlswv))]))
         den = np.sum(vlswv)
@@ -25,11 +30,11 @@ class Vls:
             self.vs += [np.uint64(den)]
         return self
 
-    def set_initState(self,state):
+    def set_initState(self: VlsType,state: bool) -> VlsType:
         self.initState = state
         return self
 
-    def print_v(self):
+    def print_v(self: VlsType) -> VlsType:
         print(self.v[:10])
         return self
 
